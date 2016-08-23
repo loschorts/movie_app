b3b790e6e856c735fd2b66c6771618fcc2947eb5
Ken Tran <loschorts@gmail.com>
2016-08-23 16:21:38 -0700

    webpac config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-  entry: "frontend/entry.jsx",
+  entry: "path/to/entry.jsx",
   output: {
-      path: "app/assets/javascripts",
-      filename: "bundle.js"
+    path: "app/assets/javascripts",
+    filename: "bundle.js",
   },
   module: {
-	  loaders: [
-	    {
-	      test: [/\.jsx?$/, /\.js?$/], // specifies file types to transpile
-	      exclude: /(node_modules)/, // leaves dependencies alone
-	      loader: 'babel', // sets Babel as the transpiler
-	      query: {
-	        presets: ['es2015', 'react'] // tells Babel what syntaxes to translate
-	      }
-	    }
-	  ]
-	},
-	devtool: 'source-map'
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
 };
