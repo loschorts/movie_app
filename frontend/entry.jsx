f3f5b2dc5650642099148fd338e1fca4156ba818
Ken Tran <loschorts@gmail.com>
2016-08-23 16:56:56 -0700

    frontend

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
 document.addEventListener('DOMContentLoaded', ()=> {
 	const root = document.querySelector('#root');
-	ReactDOM.render(<h1>HELLO</h1>, root);
+	ReactDOM.render(App, root);
 });
